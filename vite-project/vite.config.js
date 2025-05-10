749c2b292e3004686cf51b8eaeca2eb4f2bf6e23
Nidhekshaa <nknidhekshaa@gmail.com>
2025-05-10 15:34:18 +0530

    changes updated

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Stopwatch App',
+        short_name: 'Stopwatch',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/stopwatch.jpeg',
+            sizes: '192x192',
+            type: 'image/jpeg',
+          },
+          {
+            src: '/stopwatch.jpeg',
+            sizes: '512x512',
+            type: 'image/jpeg',
+          },
+        ],
+      },
+    }),
+  ],
+});
